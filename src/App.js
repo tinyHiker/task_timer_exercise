16196ea04b9d0f769caea017ce3cc790f4df2dce
Taha Iqbal <tiqbal10b@gmail.com>
2024-11-22 17:27:07 -0500

    New Task Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import AddTask from "./components/AddTask";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>03 - The Task Tracker</h1>
+      <h1 className="text-2xl font-bold py-4 pl-6">03 - The Task Tracker</h1>
+      <p className="text-xl pl-6">Hi there!</p>
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6">Click</p>
+        <AddTask />
+        <p className="text-xl my-2">to add a new task</p>
+      </div>
+    </>
   );
 }
 
